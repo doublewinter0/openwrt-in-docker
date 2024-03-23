5683714e7f3513ab811cb2fb01e8e340c363062f
doublewinter0 <erdong.me@gmail.com>
2024-03-23 23:36:08 +0800

    add lan dev route
    Signed-off-by: doublewinter0 <erdong.me@gmail.com>

--- bin/openwrt-lan.sh
+++ bin/openwrt-lan.sh
@@ -3,13 +3,16 @@
 func() {
   sleep 10
 
-  ip link add lan link openwrt-lan type macvlan mode bridge
+  ip link add link openwrt-lan name lan type macvlan mode bridge
 
   ip link set lan up
 
   sleep 20
 
   ip route add 192.168.1.1 dev lan
+
+  # 局域网其他设备，需要添加路由，否则无法访问
+  # ip route add 192.168.1.1 dev lan
 
   sleep 180
 
